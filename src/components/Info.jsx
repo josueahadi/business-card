e1c825a4ee046a3d321b143c78cbaa19e4a5df38
josueahadi <ahadihjosue@gmail.com>
2024-07-22 01:46:08 +0200

    feat: buttons

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -11,17 +11,29 @@ export default function Info() {
         />
       </div>
       <div className="text-center">
-        <h3 className="text-white font-bold text-2xl pt-4 ">Habib Josue</h3>
-        <h4 className="text-[#F3BF99] text-xs font-normal pt-1">
+        <h3 className="text-white font-bold text-2xl pt-6 ">Habib Josue</h3>
+        <h4 className="text-[#F3BF99] text-sm font-normal pt-1">
           Frontend Developer
         </h4>
-        <a className="text-[10px] text-[#F5F5F5] pt-6" href="">
+        <a className="text-[11px] text-[#F5F5F5] pt-6" href="">
           habibjosue.tech
         </a>
       </div>
-      <div>
-        <a href="">Email</a>
-        <a href="">LinkedIn</a>
+      <div className="flex justify-center gap-4 py-6">
+        <a
+          className="text-[#374151] font-medium text-sm bg-white w-28 py-2 space-x-2 rounded-md text-center"
+          href=""
+        >
+          <i class="fa-solid fa-envelope"></i>
+          <span>Email</span>
+        </a>
+        <a
+          className="text-white font-medium text-sm bg-[#5093E2] w-28 py-2 space-x-2 rounded-md text-center"
+          href=""
+        >
+          <i class="fa-brands fa-linkedin"></i>
+          <span>Linkedin</span>
+        </a>
       </div>
     </div>
   );
