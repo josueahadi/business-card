4f5503ba7e351d680b87da636836bf81a92c778f
Josue <ahadihjosue@gmail.com>
2024-08-19 20:29:17 +0200

    chore: add gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="w-full md:h-screen md:w-[317px] mx-auto flex items-center justify-center">
-      <main className="w-full md:h-[780px] bg-[#1A1B21] rounded-none md:rounded-xl flex flex-col">
-        <Info />
-        <About />
-        <Interests />
-        <div className="flex-grow pt-8 md:pt-0"></div>
-        <Footer />
-      </main>
+    <div className="bg-gradient-to-b from-[#bcd9e6] to-[#dcdcdf]">
+      <div className="w-full md:h-screen md:w-[317px] mx-auto flex items-center justify-center">
+        <main className="w-full md:h-[780px] bg-gradient-to-b from-[#242633] to-[#070228] rounded-none md:rounded-xl flex flex-col">
+          <Info />
+          <About />
+          <Interests />
+          <div className="flex-grow pt-8 md:pt-0"></div>
+          <Footer />
+        </main>
+      </div>
     </div>
   );
 }
